5f098c940b926fc1e1571bd5fb8073ce2598d1fd
Your Name <you@example.com>
2026-03-23 16:39:22 +0100

    Ajout CSS Barre de recherche

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,8 @@ Encore
     .addEntry('header', './assets/styles/header.css')
     .addEntry('footer', './assets/styles/footer.css')
 
-    /* Fichiers css non commit (dans .gitignore) */
-    .addEntry('test-barre-de-recherche', './assets/styles/components/barre-de-recherche.css')
+    
+    .addEntry('barre-de-recherche', './assets/styles/components/barre-de-recherche.css')
 
     .copyFiles({
         from: "./assets/images",
