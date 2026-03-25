6818dd1b11f92c00882765756502ca7d12c17674
Your Name <you@example.com>
2026-03-25 15:06:39 +0100

    Amelioration de settings avec le dev des autres fronts

--- src/Controller/ParametreController.php
+++ src/Controller/ParametreController.php
@@ -15,4 +15,25 @@ class ParametreController extends AbstractController
         $currentRoute = $request->attributes->get('_route'); // Récupère le nom de la route actuelle
         return $this->render('parametre/index.html.twig',['ancien_page_title' => 'Home','page_title' => $currentRoute,]);  // 'ancien_page_title' => 'Home'  <-- non dynamique
     }
+
+    #[Route('/parametres/language', name: 'Language')]   // Route pour la page de paramètres
+    public function Language(Request $request): Response
+    {
+        $currentRoute = $request->attributes->get('_route'); // Récupère le nom de la route actuelle
+        return $this->render('parametre/modifier-mdp.html.twig',['ancien_page_title' => 'Paramètres','page_title' => $currentRoute,]);  // 'ancien_page_title' => 'Paramètres'  <-- non dynamique
+    }
+
+    #[Route('/parametres/compte', name: 'Compte')]   // Route pour la page de paramètres
+    public function Compte(Request $request): Response
+    {
+        $currentRoute = $request->attributes->get('_route'); // Récupère le nom de la route actuelle
+        return $this->render('parametre/modifier-mdp.html.twig',['ancien_page_title' => 'Paramètres','page_title' => $currentRoute,]);  // 'ancien_page_title' => 'Paramètres'  <-- non dynamique
+    }
+
+    #[Route('/parametres/modifier-mdp', name: 'Modifier-mdp')]   // Route pour la page de paramètres
+    public function Modifiermdp(Request $request): Response
+    {
+        $currentRoute = $request->attributes->get('_route'); // Récupère le nom de la route actuelle
+        return $this->render('parametre/modifier-mdp.html.twig',['ancien_page_title' => 'Paramètres','page_title' => $currentRoute,]);  // 'ancien_page_title' => 'Paramètres'  <-- non dynamique
+    }
 }
