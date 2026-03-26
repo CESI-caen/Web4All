34956c67c62450affed3d93c5ef37f8ff2abde22
Your Name <you@example.com>
2026-03-26 11:30:09 +0100

    Ajout de theme js

---
+++ assets/scripts/parametre/theme.js
@@ -0,0 +1,24 @@
+const toogleButton = document.querySelector('.ThemePage');
+const body = document.body;
+
+const savedTheme = localStorage.theme;
+
+if (savedTheme) {
+    body.classList.add(savedTheme);
+}
+
+toogleButton.addEventListener('click', () => {
+    body.classList.toggle("dark");
+    const theme = body.classList.contains("dark") ? "dark" : "";
+    localStorage.theme = theme;
+});
+
+window.matchMedia("prefers-color-scheme: dark").addEventListener
+("change", (event) => {
+    body.classList.remove("dark");
+    localStorage.theme = "";
+    if (event.matches) {
+        body.classList.add("dark");
+        localStorage.theme = "dark";
+    }
+});
