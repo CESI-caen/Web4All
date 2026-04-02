93bdc20f1704a0cc1204db39321428edda95cd04
Your Name <you@example.com>
2026-04-02 16:29:38 +0200

    Mise a jour MDP dans parametres fait

--- src/Controller/ParametreController.php
+++ src/Controller/ParametreController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use App\Service\PdoService;
+use App\Model\UserModel;
 
 class ParametreController extends AbstractController
 {
@@ -38,10 +40,36 @@ class ParametreController extends AbstractController
 
     #[Route('/parametres/modifier-mdp', name: 'Modifier-mdp')]   // Route pour la page de paramètres
     public function Modifiermdp(Request $request): Response
-    {
+    {   
+        $pdo = new PdoService();
+        $userModel = new UserModel($pdo);
+
         $currentRoute = $request->attributes->get('_route'); // Récupère le nom de la route actuelle
         $user = $request->getSession()->get('user');
         $userId = $user['id'] ?? null;
-        return $this->render('parametre/modifier-mdp.html.twig',['ancien_page_title' => 'Paramètres','page_title' => $currentRoute, 'userId' => $userId, 'user' => $user]);  // 'ancien_page_title' => 'Paramètres'  <-- non dynamique
+
+        $user_bdd = $userModel->getUserByEmail($user['email']);
+
+        $mdp_actuel = $request->request->get('current_password');
+        $nouveau_mdp_1 = $request->request->get('password');
+        $nouveau_mdp_2 = $request->request->get('password_confirm');
+
+        $message = null;
+         
+
+        if ($request->isMethod('POST')) {
+            if ($user_bdd && $user_bdd['Mdp'] === $mdp_actuel) { //password_verify($mdp_actuel, $user_bdd['Mdp'])
+                
+                if ($nouveau_mdp_1 === $nouveau_mdp_2) { 
+                    $userModel->updateUserPassword($userId, $nouveau_mdp_1);
+                    return $this->redirectToRoute('Deconnexion');
+                }else {
+                $message = "Les mots de passe ne correspondent pas  !";
+                }
+            }else{
+                $message = "Mot de passe actuel incorrect !";
+            }
+        }
+    return $this->render('parametre/modifier-mdp.html.twig',['ancien_page_title' => 'Paramètres','page_title' => $currentRoute, 'userId' => $userId, 'user' => $user, 'message' => $message]);
     }
 }
