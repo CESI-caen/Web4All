fbc303af89f2308aa4e073040c0b03a4bc156c0d
Your Name <you@example.com>
2026-04-03 09:58:33 +0200

    Correction model postuler

--- src/Model/PostulerModel.php
+++ src/Model/PostulerModel.php
@@ -5,7 +5,6 @@ use App\Service\PdoService;
 
 class PostulerModel
 // Modèle de la Postuler = noter une offre, qui représente la relation entre un utilisateur et une offre : "un étudiant peut postuler à une offre"
-class PostulerModel
 {
     private \PDO $pdo;
 
